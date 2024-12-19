ec850c354cea0c06161adfca5c3a93413ec38d68
benjimosso <benjimosso@gmail.com>
2024-12-19 15:13:03 -0800

    imagenes para la seccion de ayuda al cliente.

--- app/Components/AyudaClientes.tsx
+++ app/Components/AyudaClientes.tsx
@@ -1,56 +1,71 @@
-'use client'
+"use client";
 import Image from "next/image";
+// import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { useState } from "react";
 
 const asesorias = [
   {
     id: 1,
     nombre: "Divorcios",
-    descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.11111",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.11111",
+    image: "/images/divorcio.jpg",
   },
   {
     id: 2,
     nombre: "Defensa Criminal",
     descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.2222",
+    image: "/images/defensaCriminal.jpg",
   },
   {
     id: 3,
-    nombre: "Defensa de Inmigracion",
+    nombre: "Custodia de hijos",
     descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.3333",
+    image: "/images/custodia.jpg",
   },
 ];
 
-function handleClick(id: number) {
-  console.log(`Clicked on asesoria with id: ${id}`);
-  const paragraph = document.getElementById("Paragraph");
-  if (paragraph) {
-    paragraph.innerText = asesorias[id - 1].descripcion ;
-  }
-    
-}
-
 export default function AyudaClientes() {
+  const [selectedAsesoria, setSelectedAsesoria] = useState<number>(1);
+
   return (
-    <div className="grid grid-rows-1 gap-4">
-      <h1>Como Ayudamos a nuestros Clientes?</h1>
-      <div className="grid grid-cols-3 gap-4">
+    <div className="flex flex-col  bg-lightWhite w-[100%] min-h-[800px]  px-20 py-16">
+      <h1 className="text-4xl font-bold ">
+        Como Ayudamos a nuestros Clientes?
+      </h1>
+      <div className="flex flex-col gap-4 mt-8">
+      <div className="flex gap-8">
         {asesorias.map((asesoria) => (
           <div
             key={asesoria.id}
-            className="flex flex-col items-center p-4 border border-solid border-lightgreen rounded-3xl hover:bg-argblue hover:opacity-75 cursor-pointer font-medium"
-            onClick={() => handleClick(asesoria.id)}
+            className={`flex flex-col items-center justify-center max-w-72 max-h-11 p-4  rounded-3xl cursor-pointer font-light ${selectedAsesoria === asesoria.id ? "bg-argblue" : "bg-lightgreen"}`}
+            onClick={() => setSelectedAsesoria(asesoria.id)}
           >
-            <h2 >{asesoria.nombre}</h2>
+            <p>{asesoria.nombre}</p>
           </div>
         ))}
       </div>
-      <div className="grid grid-cols-2 gap-4 min-h-{900px} border border-solid border-lightgreen rounded-3xl p-10">
+      <div className="grid grid-cols-2 border-2 border-solid border-lightgreen rounded-3xl p-10">
+        <div className="flex flex-col gap-10 ">
+          <h1 className="text-3xl font-bold ">
+            {asesorias[selectedAsesoria - 1].nombre}
+          </h1>
+          <p className="max-w-72 ">
+            {asesorias[selectedAsesoria - 1].descripcion}
+          </p>
+        </div>
         <div >
-            <h1 className="text-xl font-bold ">Title</h1>
-            <p id="Paragraph">Paragraph</p>
+          {/* <AspectRatio ratio={16 / 9}> */}
+            <Image
+              src={asesorias[selectedAsesoria - 1].image}
+              alt={asesorias[selectedAsesoria - 1].nombre}
+              width={450}
+              height={253}
+              className="rounded-xl "
+            />
+          {/* </AspectRatio> */}
         </div>
-        <div>
-            <Image src="/next.svg" alt="Johnny Abogado" width={500} height={500} className="rounded-xl"/>
-        </div>
+      </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import AyudaClientes from "../app/Components/AyudaClientes";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen  pb-20 gap-16  font-[family-name:var(--font-geist-sans)]">
+    <div>
       
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
        
